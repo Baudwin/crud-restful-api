db16cda7a8d738cd5f3215b0fff1ccb89c27ae75
Titah Baudwin <baudwin12@gmail.com>
2023-10-18 19:41:41 +0100

    latest commit

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
 const db = require("./config/db")
-const PORT = 4001
+const PORT = 10000
 const books = require("./routes/books")
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-
+app.get("/", async(req,res)=>{
+    let cmd = `select * from books`
+    try {
+        let [tables] = await db.query(cmd)
+        res.json(tables)
+    } catch (error) {
+        res.send(error)
+    }
+})
 
 app.use("/api/v1", books)
 
